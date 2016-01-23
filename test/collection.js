3bbbdab5924d6fed8de751646d9d4081f7bd85a0
Oliver.Wang <ole3021@ole3021.local>
2016-01-23 17:39:53 +0800

    add collection test part 1

--- test/collection.js
+++ test/collection.js
@@ -4,7 +4,21 @@ var meld = require('../lib/index');
 var expect = chai.expect;
 
 describe ('Collection', function() {
+  var iterator;
+  var testArray = ['a','b','cd'];
+
+  beforeEach(function() {
+    iterator = 0;
+  })
+
   describe('#each(collection)', function() {
-    // TODO: test
+    it('should return each element key & value', function() {
+      meld.each(testArray, function(value, key, collection) {
+        expect(value).to.equal(testArray[iterator]);
+        expect(key).to.equal(testArray.indexOf(testArray[iterator]));
+        iterator += 1;
+        expect(collection).to.equal(testArray);
+      });
+    });
   });
 });
